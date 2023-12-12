25d65b45195115d32045558d29f4d80538351df5
Mickdevv <michael@devereux.fm>
2023-12-12 18:00:11 +0100

    Finished 33. JWT Customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,10 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import	ProductSerializer
+from .serializers import ProductSerializer
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 # Create your views here.
 @api_view(['GET'])
@@ -27,6 +30,19 @@ def getRoutes(request):
     ]
     return Response(routes)
     # return JsonResponse(routes, safe=False)
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getProducts(request):
