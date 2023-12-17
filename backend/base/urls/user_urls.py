9dc6f470c4035c4bbafc6c42e5151952a5cc95ea
Mickdevv <michael@devereux.fm>
2023-12-17 12:40:45 +0100

    Update user profile functionnality added (43)

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from base.views import user_views as views
 
 urlpatterns = [
-    path('', views.getUsers, name='users-profile'),
+    path('', views.getUsers, name='users'),
+    
     path('profile/', views.getUserProfile, name='users-profile'),
+    path('profile/update/', views.updateUserProfile, name='users-profile-update'),
     
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
